bfc9721c29db89d7045f02638885e3cf4551f590
Rigoberto Saenz <beto456789@gmail.com>
2019-06-22 21:35:56 -0500

    - First project version added - Basic shooting implemented

---
+++ AsteraX UCP C01 V07 - Movement Challenge/Assets/__Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Bullet : MonoBehaviour
+{
+    public float lifetime = 2.0f;
+    public float speed = 20.0f;
+
+
+    void Awake()
+    {
+        // Destroys the bullet after an specified time
+        Destroy(this.gameObject, lifetime);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
---
+++ AsteraX UCP C01 V07 - Movement Challenge/Assets/__Scripts/Ship.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Ship : MonoBehaviour
+{
+
+    private GameObject bulletPrefab;
+    private Transform bulletContainer;
+
+    void Awake()
+    {
+        bulletPrefab = Resources.Load<GameObject>("Prefabs/Bullet");
+        bulletContainer = GameObject.Find("BulletContainer").transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fire
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+
+    private void Fire()
+    {
+        GameObject newBullet = Instantiate(bulletPrefab, bulletContainer, true);
+        newBullet.transform.position = transform.position;
+    }
+}
